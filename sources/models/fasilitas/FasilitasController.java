6400f2f1c05748f38b91753dab7fd650e228259c
NotHydra <rizky.irswanda115@gmail.com>
2024-04-27 15:55:53 +0800

    feat: fasilitas controller initialize

---
+++ sources/models/fasilitas/FasilitasController.java
@@ -0,0 +1,89 @@
+package models.fasilitas;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class FasilitasController {
+
+	@FXML
+	private Label labelProfile;
+
+	@FXML
+	private TableView<?> tableMain;
+
+	@FXML
+	private TableColumn<?, ?> tableMainColumnDibuat;
+
+	@FXML
+	private TableColumn<?, ?> tableMainColumnDiubah;
+
+	@FXML
+	private TableColumn<?, ?> tableMainColumnKeterangan;
+
+	@FXML
+	private TableColumn<?, ?> tableMainColumnNama;
+
+	@FXML
+	private TextField textFieldDibuat;
+
+	@FXML
+	private TextField textFieldDiubah;
+
+	@FXML
+	private TextField textFieldKeterangan;
+
+	@FXML
+	private TextField textFieldNama;
+
+	@FXML
+	void onActionFasilitas(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onActionHapus(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onActionLogout(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onActionPeminjaman(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onActionPengguna(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onActionRuangan(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onActionTambah(ActionEvent event) {
+
+	}
+
+	@FXML
+	void onActionUbah(ActionEvent event) {
+
+	}
+
+	@FXML
+	void tableMainItemClick(MouseEvent event) {
+
+	}
+
+}

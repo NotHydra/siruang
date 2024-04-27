d6fde4d640c82a69fbe2a93391643345a1b49339
JUANRESQ <juan.risq@gmail.com>
2024-04-27 16:28:32 +0800

    camelcase ruangan

--- sources/models/ruangan/RuanganModel.java
+++ sources/models/ruangan/RuanganModel.java
@@ -4,63 +4,63 @@ import java.sql.Timestamp;
 import global.base.BaseModel;
 
 public class RuanganModel {
-	private final String Nama;
-	private final String Deskirpsi;
-	private final int Kapasitas;
-	private final Timestamp TanggalDibuat;
-	private final Timestamp TanggalDiubah;
+	private final String nama;
+	private final String deskirpsi;
+	private final int kapasitas;
+	private final Timestamp dibuat;
+	private final Timestamp diubah;
 	
-	public RuanganModel(String Nama, String Deskirpsi, int Kapasitas) {
+	public RuanganModel(String nama, String deskirpsi, int kapasitas) {
 		super(-1);
-		validate(Nama, Deskirpsi, Kapasitas);
+		validate(nama, deskirpsi, kapasitas);
 		
-		this.Nama = Nama;
-		this.Deskirpsi = Deskirpsi;
-		this.Kapasitas = Kapasitas;
-		this.TanggalDibuat = null;
-		this.TanggalDiubah = null;
+		this.nama = nama;
+		this.deskirpsi = deskirpsi;
+		this.kapasitas = kapasitas;
+		this.dibuat = null;
+		this.diubah = null;
 	}
 	
-	public RuanganModel(int id, String Nama, String Deskirpsi,int Kapasitas,  Timestamp TanggalDibuat, Timestamp TanggalDiubah) {
+	public RuanganModel(int id, String nama, String deskirpsi,int kapasitas,  Timestamp dibuat, Timestamp diubah) {
 		super(id);
 
-		validate(id, Nama, Deskirpsi, Kapasitas, TanggalDibuat, TanggalDiubah);
+		validate(id, nama, deskirpsi, kapasitas, dibuat, diubah);
 
-		this.Nama = Nama;
-		this.Deskirpsi = Deskirpsi;
-		this.TanggalDibuat = TanggalDibuat;
-		this.TanggalDiubah = TanggalDiubah;
+		this.nama = nama;
+		this.deskirpsi = deskirpsi;
+		this.dibuat = dibuat;
+		this.diubah = diubah;
 	}
 
 
-	private void validate(String Nama, String Deskirpsi, int Kapasitas) {
-		if (Nama == null ||  Nama.trim().isEmpty()) { 
+	private void validate(String nama, String deskirpsi, int kapasitas) {
+		if (nama == null ||  nama.trim().isEmpty()) { 
 			throw new IllegalArgumentException("Nama tidak boleh kosong");
 		}
-		if (Deskirpsi == null ||  Deskirpsi.trim().isEmpty()) { 
+		if (deskirpsi == null ||  deskirpsi.trim().isEmpty()) { 
 			throw new IllegalArgumentException("Deskripsi tidak boleh kosong");
 		}
-		if (Kapasitas == 0 ||  Kapasitas == 0) { 
+		if (kapasitas == 0 ||  kapasitas == 0) { 
 			throw new IllegalArgumentException("Kapasitas tidak boleh kosong");
 		}
 		
 	}
 
 	public String getnama() {
-		return this.Nama;
+		return this.nama;
 	}
 	public String getDeskripsi() {
-		return this.Deskirpsi;
+		return this.deskirpsi;
 	}
 	public int getKapasitas() {
-		return this.Kapasitas;
+		return this.kapasitas;
 	}
 	public Timestamp getTanggalDibuat() {
-		return this.TanggalDibuat;
+		return this.dibuat;
 	}
 
 	public Timestamp getTanggalDiubah() {
-		return this.TanggalDiubah;
+		return this.diubah;
 	}
 
 	@Override
